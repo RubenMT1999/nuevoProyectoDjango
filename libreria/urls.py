6e6c9b5dddff1ecd788ecb4f1bf0e8a9b65abd97
rubenmt <rmatiastiscar@safareyes.es>
2022-04-07 17:44:55 +0200

    segundo commit

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from libreria.views import mostrar_inicio
+from libreria.views import mostrar_inicio, crear_objeto
 
 urlpatterns = [
     path('',mostrar_inicio),
     #path('questions/',),
-    #path('questions/crear',)
+    path('crear/',crear_objeto)
 ]
--- libreria/views.py
+++ libreria/views.py
@@ -1,6 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 
 def mostrar_inicio(request):
-    return render(request,'inicio.html')
+
+    lista = Question.objects.all()
+    return render(request,'inicio.html',{'Cuestiones':lista})
+
+
+def crear_objeto(request):
+
+    #GET
+    if request.method == 'GET':
+        return render(request,'formulario.html')
+
+    #POST
+    else:
+        objeto_nuevo = Question()
+        objeto_nuevo.question_text = request.POST.get('id')
+        objeto_nuevo.pub_date = request.POST.get('nombre')
+        Question.save(objeto_nuevo)
+        return render(request,mostrar_inicio)
